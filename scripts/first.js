114ee951abf31bf822196870d1830f29203931d5
Rogue User <sarath.h@xminds.com>
2024-01-31 16:29:45 +0530

    default parameters

--- scripts/first.js
+++ scripts/first.js
@@ -1,17 +1,22 @@
 console.log("program started")
 
+// constant as a function
+const seed = () => equation(2342, 3423, undefined);
+
 // Normal function
-normalFunction = (function(){
+normalFunction = function () {
+    console.log("inside normal function")
     return "<Not an ARROW FUNCTION>";
-})
+}
 console.log(normalFunction())
 
 // Arrow function
-arrowFunction = (() => {
+var arrowFunction = () => {
+    console.log("inside arrow function")
     return "<ARROW FUNCTION>";
-})
+}
 console.log(arrowFunction())
 
-// Single line Arrow function
-addition = (a, b) => a + b;
-console.log(addition(3,4))
+// Single line Arrow function with default parameter
+let equation = (a = 1000, b = 1000, difference = a - b) => (a + b) * Math.abs(difference);
+console.log(seed())
