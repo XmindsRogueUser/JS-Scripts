a0c3e98853f743e5152b0dc20a339450d34d9faf
Rogue User <sarath.h@xminds.com>
2024-02-01 12:45:19 +0530

    learned classes

--- scripts/first.js
+++ scripts/first.js
@@ -1,13 +1,13 @@
 console.log("program started")
 
-// constant as a function
+// constant as a function (a random number)
 const seed = () => equation(2342, 3423, undefined);
 // constant object
 const itemObj = {
-    DGS: 345,
-    TYI: 677,
-    RTL: 342,
-    QRF: 580,
+    Fruites: 345,
+    Vegitables: 677,
+    Grains: 342,
+    Spices: 580,
 };
 
 // Single line Arrow function with default parameter
@@ -20,38 +20,84 @@ normalFunction = function () {
 }
 console.log(normalFunction());
 
-class World {
+// parent class
+class WorldProcessor {
+    addBonusItem = (seed, item) => {
+        if (seed < 6000000) {
+            item.set("Bonus", 1);
+        } else {
+            item.delete("Spices");
+        }
+        return item;
+    }
+
+}
+
+// Class defenition inheriting WorldProcessor
+class World extends WorldProcessor {
+    // constructor
     constructor(seed, itemMap) {
+        // initialize parent before child
+        super();
         this.seed = seed;
         this.items = itemMap;
+        this.processItems(itemMap);
+        // calling parent class function
+        this.items = this.addBonusItem(this.getSeed, this.getItems)
+    }
+
+    // defining a method in child class
+    processItems = (itemMap) => {
         //looping through map
         for (let [key, value] of itemMap) {
-            if (key === "QRF") {
-                this.items.set("QRF", 0)
+            if (key === "Spices") {
+                this.items.set("Spices", 0)
             }
         }
+    }
 
+    // seed getter
+    get getSeed() {
+        return this.seed;
+    }
+
+    // item getter
+    get getItems() {
+        return this.items;
+    }
+
+
+    // seed setter
+    set setSeed(seed) {
+        this.seed = seed;
+    }
+    // item setter
+    set setItems(items) {
+        this.items = items;
     }
 }
 
 // Arrow function
 let arrowFunction = () => {
-    console.log("inside arrow function")
+    console.log("inside an arrow function")
 
     let itemMap = new Map();
-    itemMap.set("DGS", itemObj.DGS);
-    itemMap.set("TYI", itemObj.TYI);
-    itemMap.set("RTL", itemObj.RTL);
-    itemMap.set("QRF", itemObj.QRF);
+    itemMap.set("Fruites", itemObj.Fruites);
+    itemMap.set("Vegitables", itemObj.Vegitables);
+    itemMap.set("Grains", itemObj.Grains);
+    itemMap.set("Spices", itemObj.Spices);
     let world = new World(seed(), itemMap);
+    // forEach loop
     world.items.forEach((v, k) => {
         console.log(k + ", " + v);
     })
 
-    //learn sets 
+    // learn sets
     const setOfNumbers = new Set(itemMap.values());
-    setOfNumbers.add(itemObj.DGS);
-    if (setOfNumbers.size != 4)
+    // adding duplicate entry to set
+    setOfNumbers.add(itemObj.Fruites);
+    // checking if the set size changed
+    if (setOfNumbers.size != 3)
         throw new Error("Unexpected behaviour expressed by set");
 
     // array using constructor notation
@@ -66,4 +112,6 @@ let arrowFunction = () => {
     console.log(arrayItems);
     return "seed = " + world.seed + ", item size = " + world.items.size;
 }
+
+// run the program
 console.log(arrowFunction());
