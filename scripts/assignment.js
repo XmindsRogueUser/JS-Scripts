5ee3b04aee17fae33f6cc9cf747ae8a8f5a15646
Rogue User <sarath.h@xminds.com>
2024-02-01 19:07:31 +0530

    added recursion check

--- scripts/assignment.js
+++ scripts/assignment.js
@@ -12,21 +12,6 @@
 // contains equivalent property values as in the second one?
 
 
-class Student {
-    constructor(rollno, course, duration, startDate, endDate, garde) {
-        this.rollno = rollno;
-        this.course = course;
-        this.duration = duration;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.garde = garde;
-    }
-
-    getStudentData() {
-        console.log(Object.entries(this));
-    }
-}
-
 const defaultStudent = {
     rollno: 27,
     course: "Computer science",
@@ -40,6 +25,7 @@ const defaultStudent = {
     get getStartDate() { return this.startDate },
     get getEndDate() { return this.endDate },
     get getGarde() { return this.garde },
+    get getStudentData() { return this },
     set setRollno(rollno) { return this.rollno = rollno },
     set setCourse(course) { return this.course = course },
     set setDuration(duration) { return this.duration = duration },
@@ -48,54 +34,63 @@ const defaultStudent = {
     set setGarde(garde) { return this.garde = garde }
 };
 
-setStudentData = (
-    rollno,
-    course,
-    duration,
-    startDate,
-    endDate,
-    garde
-) =>
-    new Student(rollno, course, duration, startDate, endDate, garde);
+const defaultNestedObj = {
+    name: "John",
+    age: 15
+}
 
 function isEqual(object1, object2) {
+
+    // return false if both obj are of same type
+    if (typeof object1 != typeof object2) return false;
+
     const objKeys1 = Object.keys(object1);
     const objKeys2 = Object.keys(object2);
-
+    // return false if key length is different
     if (objKeys1.length !== objKeys2.length) {
         return false;
     }
-
     for (let key of objKeys1) {
+        // compare if value is a nested object
+        if (typeof object1[key] === 'object' && object1[key] !== null) {
+            if (!isEqual(object1[key], object2[key])) {
+                return false;
+            }
+        }
+        // compare if value is a literal
         if (object1[key] !== object2[key]) {
             return false;
         }
     }
-
     return true;
 }
 
 result = () => {
-    student1 = setStudentData(
-        defaultStudent.getRollno,
-        defaultStudent.getCourse,
-        defaultStudent.getDuration,
-        defaultStudent.getStartDate,
-        defaultStudent.getEndDate,
-        defaultStudent.getGarde
-    );
+    // object 1
+    let student1 = new Object();
+    student1.rollno = defaultStudent.getRollno;
+    student1.course = defaultStudent.getCourse;
+    student1.duration = defaultStudent.getDuration;
+    student1.startDate = defaultStudent.getStartDate;
+    student1.endDate = defaultStudent.getEndDate;
+    student1.garde = defaultStudent.getGarde;
+    // introducing nested object
+    student1.nestedObject = defaultNestedObj;
 
-    student2 = setStudentData(
-        defaultStudent.getRollno,
-        defaultStudent.getCourse,
-        defaultStudent.getDuration,
-        defaultStudent.getStartDate,
-        defaultStudent.getEndDate,
-        defaultStudent.getGarde
-    );
-    student1.getStudentData();
-    student2.getStudentData();
+    // object 2
+    let student2 = new Object();
+    student2.rollno = defaultStudent.getRollno;
+    student2.course = defaultStudent.getCourse;
+    student2.duration = defaultStudent.getDuration;
+    student2.startDate = defaultStudent.getStartDate;
+    student2.endDate = defaultStudent.getEndDate;
+    student2.garde = defaultStudent.getGarde;
+    // introducing nested object
+    student2.nestedObject = defaultNestedObj;
 
+    console.log(defaultStudent.getStudentData);
+
+    // equals check
     if (isEqual(student1, student2)) {
         return "The Objects are the same";
     }
