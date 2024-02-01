417a661ec7d027ee4cfb4fbf8cf112c9b15f77fe
Rogue User <sarath.h@xminds.com>
2024-02-01 13:35:37 +0530

    Learned Objects

---
+++ scripts/second.js
@@ -0,0 +1,37 @@
+var subject = {
+    name: "Maths",
+    teacher: "Doe",
+    id: 1,
+    emailOfTeacher: "johndoe@gmail.com",
+    getId: function () {
+        return this.name + '_' + this.id
+    },
+    marks: {
+        firstSem: 0,
+        secondSem: 0
+    }
+};
+
+function main() {
+    var student = new Object();
+    // Attach properties and methods to student object     
+    student.firstName = "Charlie";
+    student["lastName"] = "Delta";
+    student.age = 15;
+    student.email = "charliedelta@gmail.com";
+    student.getFullName = function () {
+        return this.firstName + ' ' + this.lastName;
+    };
+    if (subject.hasOwnProperty("marks")) {
+        student.mark = subject.marks.firstSem + subject.marks.secondSem;
+    }
+    Object.freeze(student);
+    Object.seal(student);
+    // this should not work since the obj is sealed and frozen
+    student.mark = 100;
+    student.newField = "newData";
+
+    return student;
+}
+let studentDetails = main()
+console.log("Details of student " + studentDetails.getFullName() + "\n" + JSON.stringify(studentDetails));
