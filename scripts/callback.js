8c848ea1a406e78cf735726c6586bf4ddebe6441
Rogue User <sarath.h@xminds.com>
2024-02-06 10:25:18 +0530

    callback

--- scripts/callback.js
+++ scripts/callback.js
@@ -1,11 +1,19 @@
 function download(url, callback) {
     setTimeout(() => {
         // script to download the picture here
-        console.log(`Downloading ${url} ...`);
+        console.log(`process 1:  Downloading ${url} ...`);
 
         // process the picture once it is completed
         callback(url);
     }, 1000);
+
+    // script to download the picture here
+    console.log(`process 2:  Downloading ${url} ...`);
+
+    // process the picture once it is completed
+    callback(url);
+
+
 }
 
 download("http://www.google.com/lpl.img", (url) => {
