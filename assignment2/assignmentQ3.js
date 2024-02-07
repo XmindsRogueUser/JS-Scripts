c5c8708c5bc4cfa46b306e77916156f4dfb111db
Rogue User <sarath.h@xminds.com>
2024-02-07 15:16:08 +0530

    q3 removed async fetch function

--- assignment2/assignmentQ3.js
+++ assignment2/assignmentQ3.js
@@ -3,26 +3,22 @@
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-// call api
-fetchApiResponse = async () => {
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
 // Extract 'name' fields
 displayAllNameProperty = ({ name, company: { name: compayName } }) => {
     return [name, compayName];
 }
 
 main = async () => {
-    let data = await fetchApiResponse();
+    let data;
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        data = await response.json();
+    } catch (error) {
+        console.error('Error:', error);
+    };
     let resultList = []
     data.forEach(element => {
         resultList.push(...displayAllNameProperty(element))
